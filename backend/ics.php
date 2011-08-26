8baa3604751c8e98e016411a2243962379a8a081
Wolfram Quester <wolfi@sigxcpu.org>
2011-08-26 19:11:35 +0200

    Imported Upstream version 1.5.4

--- backend/ics.php
+++ backend/ics.php
@@ -1668,11 +1668,13 @@ class PHPContentsImportProxy extends MAPIMapping {
 
         // Override 'body' for truncation
         $body = mapi_openproperty($mapimessage, PR_BODY);
-        if(strlen($body) > $truncsize) {
+        $message->bodysize = strlen($body);
+
+        if($message->bodysize > $truncsize) {
             $body = utf8_truncate($body, $truncsize);
             $message->bodytruncated = 1;
-            $message->bodysize = strlen($body);
-        } else {
+        }
+        else {
             $message->bodytruncated = 0;
         }
 
@@ -1900,6 +1902,8 @@ class PHPContentsImportProxy extends MAPIMapping {
                 return 512;
             case SYNC_TRUNCATION_1K:
                 return 1024;
+            case SYNC_TRUNCATION_2K:
+                return 2*1024;
             case SYNC_TRUNCATION_5K:
                 return 5*1024;
             case SYNC_TRUNCATION_SEVEN:
@@ -2628,7 +2632,7 @@ class BackendICS {
         $items['searchtotal'] = $querycnt;
 
         if ($querycnt > 0)
-            $abentries = mapi_table_queryrows($table, array(PR_ACCOUNT, PR_DISPLAY_NAME, PR_SMTP_ADDRESS, PR_BUSINESS_TELEPHONE_NUMBER, PR_GIVEN_NAME, PR_SURNAME, PR_MOBILE_TELEPHONE_NUMBER, PR_HOME_TELEPHONE_NUMBER), $rangestart, $querylimit);
+            $abentries = mapi_table_queryrows($table, array(PR_ACCOUNT, PR_DISPLAY_NAME, PR_SMTP_ADDRESS, PR_BUSINESS_TELEPHONE_NUMBER, PR_GIVEN_NAME, PR_SURNAME, PR_MOBILE_TELEPHONE_NUMBER, PR_HOME_TELEPHONE_NUMBER, PR_TITLE, PR_COMPANY_NAME, PR_OFFICE_LOCATION), $rangestart, $querylimit);
 
         for ($i = 0; $i < $querylimit; $i++) {
             $items[$i][SYNC_GAL_DISPLAYNAME] = w2u($abentries[$i][PR_DISPLAY_NAME]);
@@ -2636,7 +2640,7 @@ class BackendICS {
             if (strlen(trim($items[$i][SYNC_GAL_DISPLAYNAME])) == 0)
                 $items[$i][SYNC_GAL_DISPLAYNAME] = w2u($abentries[$i][PR_ACCOUNT]);
 
-            $items[$i][SYNC_GAL_ALIAS] = $items[$i][SYNC_GAL_DISPLAYNAME];
+            $items[$i][SYNC_GAL_ALIAS] = $items[$i][PR_ACCOUNT];
             //it's not possible not get first and last name of an user
             //from the gab and user functions, so we just set lastname
             //to displayname and leave firstname unset
@@ -2652,7 +2656,7 @@ class BackendICS {
             $items[$i][SYNC_GAL_EMAILADDRESS] = w2u($abentries[$i][PR_SMTP_ADDRESS]);
             //check if an user has an office number or it might produce warnings in the log
             if (isset($abentries[$i][PR_BUSINESS_TELEPHONE_NUMBER]))
-                $items[$i][SYNC_GAL_OFFICE] = w2u($abentries[$i][PR_BUSINESS_TELEPHONE_NUMBER]);
+                $items[$i][SYNC_GAL_PHONE] = w2u($abentries[$i][PR_BUSINESS_TELEPHONE_NUMBER]);
             //check if an user has a mobile number or it might produce warnings in the log
             if (isset($abentries[$i][PR_MOBILE_TELEPHONE_NUMBER]))
                 $items[$i][SYNC_GAL_MOBILEPHONE] = w2u($abentries[$i][PR_MOBILE_TELEPHONE_NUMBER]);
@@ -2660,8 +2664,15 @@ class BackendICS {
             if (isset($abentries[$i][PR_HOME_TELEPHONE_NUMBER]))
                 $items[$i][SYNC_GAL_HOMEPHONE] = w2u($abentries[$i][PR_HOME_TELEPHONE_NUMBER]);
 
-            if (isset($abentries[$i][PR_ACCOUNT]))
-                $items[$i][SYNC_GAL_COMPANY] = w2u($abentries[$i][PR_ACCOUNT]);
+            if (isset($abentries[$i][PR_COMPANY_NAME]))
+                $items[$i][SYNC_GAL_COMPANY] = w2u($abentries[$i][PR_COMPANY_NAME]);
+
+            if (isset($abentries[$i][PR_TITLE]))
+                $items[$i][SYNC_GAL_TITLE] = w2u($abentries[$i][PR_TITLE]);
+
+            if (isset($abentries[$i][PR_OFFICE_LOCATION]))
+                $items[$i][SYNC_GAL_OFFICE] = w2u($abentries[$i][PR_OFFICE_LOCATION]);
+
         }
         return $items;
     }
@@ -2861,8 +2872,12 @@ class BackendICS {
                 else
                     $this->_storeAttachment($mapimessage, $part);
             }
-        } else {
-            $body = u2wi($message->body);
+        }
+        else if($message->ctype_primary == "text" && $message->ctype_secondary == "html") {
+            $body_html .= u2wi($message->body);
+        }
+        else {
+             $body = u2wi($message->body);
         }
 
         // some devices only transmit a html body
